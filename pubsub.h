15658511bf34bc0854621b64662b9e29aa5d9c1d
alex <u@ubuntu-14.04vm>
2016-12-22 13:50:03 +0800

    subscribe and notify

--- pubsub.h
+++ pubsub.h
@@ -1,4 +1,48 @@
 #ifndef __PUBSUB__
 #define __PUBSUB__
+#include <unordered_set>
+#include <type_traits>
+
+namespace PubSub {
+
+
+    template <typename T> 
+    class Subscriber {
+    public:
+        Subscriber(){}
+        virtual ~Subscriber() {}
+
+        virtual void onNotified(const T& e)=0;
+        
+        template<typename T2> 
+        void subscribe() {
+          subscribersSet.insert(this); 
+        }
+        template<typename T2> 
+        void unsubscribe() {
+          subscribersSet.erase(this); 
+        }
+    private:
+        static std::unordered_set<Subscriber<T>*> subscribersSet;
+            
+        template <typename T2>                
+        friend void notify (const T2& e); 
+    };
+
+
+    template <typename T>
+    std::unordered_set<Subscriber<T>*> Subscriber<T>::subscribersSet;
+
+    template <typename T> 
+    void notify(const T& e) {
+
+        for (auto& sub: Subscriber<T>::subscribersSet) {
+            sub->onNotified(e);
+        }
+
+    }
+
+} // namespace PubSub
+
 
 #endif //__PUBSUB__
--- tests.cpp
+++ tests.cpp
@@ -1,5 +1,49 @@
 #include "gmock/gmock.h"
 #include "pubsub.h"
+
+using namespace std;
+using namespace PubSub;
+using namespace testing;
+
+TEST(PubSub, registerAndNotify ) {
+
+    struct Event {
+        Event(int v) : val{v}{}
+        int val;
+    };
+
+    class A:public Subscriber<Event> {
+    public:
+        A() : Subscriber<Event>()
+        {
+            
+        }
+        void onNotified (const Event& e)override {
+            receivedVal=e.val;
+        }
+
+        int receivedVal=0;
+    };
+
+   
+    A a; 
+
+    Event e{100};
+    a.subscribe<Event> ();
+    ASSERT_THAT(a.receivedVal,Eq(0));
+
+    notify<Event>(e);
+    ASSERT_THAT(a.receivedVal,Eq(e.val));
+
+    a.receivedVal = 0;
+    a.unsubscribe<Event>(); 
+
+    notify<Event>(e);
+    ASSERT_THAT(a.receivedVal,Eq(0));
+
+}
+
+
 int main(int argc, char *argv[])
 {
 	testing::InitGoogleMock (&argc, argv);
